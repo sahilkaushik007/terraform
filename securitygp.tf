cdf04a847d0b91e02514ce90447c389fcc362385
sahilkaushik007 <58801477+sahilkaushik007@users.noreply.github.com>
2022-06-10 22:35:48 +0530

    Update securitygp.tf

--- securitygp.tf
+++ securitygp.tf
@@ -1,16 +1,9 @@
-resource "aws_instance" "footbar" {
-    ami = "ami-079b5e5b3971bd10d"
-    instance_type = "t2.micro"
-    tags = {
-      Name = "tf"
-    }
-}
 
 
 resource "aws_instance" "footbar-new" {
     ami = "ami-079b5e5b3971bd10d"
     instance_type = "t2.micro"
-    count = 1
+    count = Servers
     tags = {
       Name = "tf-new"
     }
