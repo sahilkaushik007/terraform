5cc131560b40eaee47e0c61dc26cd2eac910e7ff
sahilkaushik007 <sahilkaushik569@gmail.com>
2022-06-10 08:59:28 +0530

    Update

--- securitygp.tf
+++ securitygp.tf
@@ -1,20 +1,9 @@
 resource "aws_instance" "footbar" {
     ami = "ami-079b5e5b3971bd10d"
     instance_type = "t2.micro"
+    region = "ap-south-1"
+    shared_credentials_file = "/home/vvdn/.aws/credentials"
     tags = {
       Name = "tf"
-    }
-}
-
-
-resource "aws_instance" "footbar-new" {
-    ami = "ami-079b5e5b3971bd10d"
-    instance_type = "t2.micro"
-    count = 1
-    tags = {
-      Name = "tf-new"
-    }
-    lifecycle {
-      prevent_destroy= true
     }
 }
